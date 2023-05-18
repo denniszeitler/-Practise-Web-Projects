55ffb090f13792a63de340f12aea35dcfa865ef1
denniszeitler <133316844+denniszeitler@users.noreply.github.com>
2023-05-18 12:54:17 +0200

    Update 3
    - bug-fix: couldn't add tasks to a new list
    - css used 3 different kind of color definitions
    - added to do

--- To-Do List/script.js
+++ To-Do List/script.js
@@ -1,28 +1,33 @@
-var input; //Input-Feld für title & task
-var taskdiv; //DIV für unordered list
-var ul; 
-var li; // li[0] = title, rest to-do Punkte
-var error; // Ausgabe in <p> Tag von Fehlermeldungen
+/*TO DO:!
+- Kommentieren :)
+- strike klasse evtl. bei addtask schon hinzufügen
+
+
+
+*/
+
+
+let input; // Input-Feld für title & task
+let error; // Ausgabe in <p> Tag von Fehlermeldungen
 
 window.onload = function() {
     input = document.getElementById("new-task");
-    taskdiv = document.getElementById("taskdiv");
-    ul = taskdiv.querySelector("ul");
-    li = document.createElement("li");
     error = document.getElementById("error");
     savedlists();
 };
 
-
-
 function addtask() {
+    const taskdiv = document.getElementById("taskdiv");
+    const ul = taskdiv.querySelector("ul");
+    const li = document.createElement("li");
     li.textContent = input.value;
-    if(input.value.length == 0){
-        error.textContent = "Es wurde kein Text eingegeben";
+
+    if (input.value.length == 0) {
+        error.textContent = "no text entered";
         return;
     }
 
-    if (ul.children.length === 0){
+    if (ul.children.length === 0) {
         input.placeholder = "Add your Tasks";
         li.textContent = input.value;
         li.classList.add("title");
@@ -32,51 +37,56 @@ function addtask() {
         ul.appendChild(li);
     }
 
-    input.value = ""; //clear Input-Feld
-};
+    input.value = ""; // clear Input-Feld
+}
 
 function savelist() {
-    var tasknr = localStorage.length;
-    var uniqueKey = 'task-' + tasknr;
-    var items = ul.querySelectorAll('li');
-    var tasks = [];
+    const taskdiv = document.getElementById("taskdiv");
+    const ul = taskdiv.querySelector("ul");
+    let tasknr = localStorage.length;
+    let uniqueKey = 'task-' + tasknr;
+    const items = ul.querySelectorAll('li');
+    let tasks = [];
 
-    if(ul.textContent.trim() === '') {
+    if (ul.textContent.trim() === '') {
         error.textContent = "list has no entries";
         return;
     }
 
-    if(ul.id.startsWith("task")) {
+    if (ul.id.startsWith("task")) {
         uniqueKey = ul.id;
     }
 
-    for(var i = 0; i < items.length; i++){
+    for (let i = 0; i < items.length; i++) {
         tasks.push(items[i].textContent);
     }
 
     localStorage.setItem(uniqueKey, JSON.stringify(tasks));
-    ul.innerHTML = ""; //clear List
+    ul.innerHTML = ""; // clear List
     input.placeholder = "Enter a title first";
     ul.removeAttribute("id");
     savedlists();
-};
+}
 
 function loadlist(uniqueKey) {
-    var requestedtask = JSON.parse(localStorage.getItem(uniqueKey));
-        input.placeholder = "Add your tasks";
-        ul.innerHTML = "";
+    const taskdiv = document.getElementById("taskdiv");
+    const ul = taskdiv.querySelector("ul");
+    const requestedtask = JSON.parse(localStorage.getItem(uniqueKey));
+    input.placeholder = "Add your tasks";
+    ul.innerHTML = "";
 
-
-    if(ul.classList.contains(uniqueKey)) {
+    if (ul.classList.contains(uniqueKey)) {
         console.log("list already opened");
     } else {
         ul.id = uniqueKey;
     }
 
-    for(var i = 0; i < requestedtask.length; i++){
+    for (let i = 0; i < requestedtask.length; i++) {
+        const li = document.createElement("li");
         li.textContent = requestedtask[i];
-        li.addEventListener('click', function(){
-            if(this.classList.contains("strike")){
+        console.log(li.textContent);
+        li.addEventListener('click', function() {
+            if (this.classList.contains("strike")) {
                 this.classList.remove("strike");
             } else {
                 this.classList.add("strike");
@@ -86,11 +96,12 @@ function loadlist(uniqueKey) {
     }
 
     ul.classList.remove(uniqueKey);
-};
+}
 
 function deletelist() {
-
-    if(ul.id == 0){
+    const taskdiv = document.getElementById("taskdiv");
+    const ul = taskdiv.querySelector("ul");
+    if (ul.id == 0) {
         error.textContent = "no list opened";
         return;
     }
@@ -98,33 +109,29 @@ function deletelist() {
     localStorage.removeItem(ul.id);
     input.placeholder = "Enter a title first";
     savedlists();
-};
+}
 
 function savedlists() {
-    var savediv = document.getElementById("save-row");
-        savediv.innerHTML = ""; //reset savedlists Liste!
+    const savediv = document.getElementById("save-row");
+    savediv.innerHTML = ""; // reset savedlists Liste!
 
     for (let key in localStorage) {
-        if (key.startsWith('task-')) {        
-            var storedlist = createStoredList(key);
+        if (key.startsWith('task-')) {
+            const storedlist = createStoredList(key);
             savediv.appendChild(storedlist);
         }
     }
-};
-
-
+}
 
 function createStoredList(key) {
-    var storedData = JSON.parse(localStorage.getItem(key));
-    var storedlist = document.createElement("div");
-        storedlist.textContent = storedData[0];
-        storedlist.href = '#';
-        storedlist.classList.add("save-object");
-        storedlist.onclick = function() {
-            loadlist(key);
+    const storedData = JSON.parse(localStorage.getItem(key));
+    const storedlist = document.createElement("div");
+    storedlist.textContent = storedData[0];
+    storedlist.href = '#';
+    storedlist.classList.add("save-object");
+    storedlist.onclick = function() {
+        loadlist(key);
     };
-        storedlist.id = key;
-        return storedlist;
-};
-
-
+    storedlist.id = key;
+    return storedlist;
+}
